82d95aea6c1dd998994a0163cdae138ca514f13e
Linkcube <poohbah125@gmail.com>
2014-03-27 03:53:53 -0700

    Fading Text
    Finally got fading text to work, text should fade in as characters have
    been, and should correctly appear, add new text, and clear all text.

--- VN Example/main.lua
+++ VN Example/main.lua
@@ -1,46 +1,75 @@
--- Visual Novel testing ground, final result should be Love Book
-
+-- Disclaimer: All software is provided as is, with no expectations. All rights are reserved by respective owners (love2d et al) as well as by the writer of this code, and all conditions of use can be changed on a whim without notification.
+-- With the above line in consideration, you may distribute this code along with your script.txt and init.txt, but let the creator know of this (he would like to know when someone has used this ;) ) 
+-- Visual Novel,Love Book
+-- Requires middleclass, may switch over to sunclass later
 -- Read first part of file and load characters/backgrounds/sounds into classes/lists and load in the load function
 local class = require 'middleclass' -- oop functionality
-local cLine = 0
-local txtDisplay = ""
-local bg = nil
-local characters = {}
-local backgrounds = {}
-local width, height = love.graphics.getDimensions()
-local line = {}
-local br = 255
-local musics = {}
-local sounds = {}
-local inpt = true
-local toFade = {}
-local toShow = {}
-local voiceTable = {}
-local voicePos = 1
-local voicePlay = false
+local cLine = 0 -- The current line being read from
+local txtDisplay = "" -- The text to display, soon to be changed
+local txtLimit = 0 -- The pixel width of text before wrapping
+local txtTable = {} -- The table to hold characters for fading in text
+local txtAlpha = {} -- Table for alpha values of txtTable
+local txtMax = 0 -- The highest count of characters in txtTable
+local bg = nil -- The background to be drawn
+local characters = {} -- The table of characters in the vn
+local backgrounds = {} -- The table of backgrounds in the vn
+local width, height = love.graphics.getDimensions() -- The initial height and width of the scren, is changed after loading in init.txt
+local line = {} -- The table of lines loaded from files
+local br = 255 -- The 'brightness' of to draw non-text
+local musics = {} -- The table of music to be played
+local sounds = {} -- The table of sounds to be played
+local inpt = true -- Whether the user input will be taken into account
+local toFade = {} -- The table of characters to fade alpha values
+local toShow = {} -- The table of characters to fade in alpha values
+local voiceTable = {} -- The table of sound files dedicated to voices
+local voicePos = 1 -- The voice file of voiceTable to play
+local voicePlay = false -- Whether voice files should be played or not
+local sX = 1 -- The scale coefficient of the x-dimension of the background
+local sY = 1 -- The scale coefficient of the y-dimension of the background
+local click = 0
+local Font = love.graphics.newFont(12)
+local oldTxt = 1
 
+--[
+--The character class for characters to be drawn, known by a name and given various values for different modifications
+--caused by functions later detailed
+--]
  local Character = class('Character')
 function Character:initialize(name, imagePath)
 	self.poses = {}
-	self.name = name
-	self.poses["default"] = love.graphics.newImage(imagePath)
-	self.image = self.poses["default"]
-	self.x = 0
-	self.y = height - self.image:getHeight()
-	self.alpha = 0
-	self.fade = 0
+	self.name = name -- The name to be called by
+	self.poses["default"] = love.graphics.newImage(imagePath) -- The default image to display
+	self.image = self.poses["default"] -- The default image to display
+	self.x = 0 -- The initial x-position to be drawn from
+	self.y = height - self.image:getHeight() -- The initial y-position to be drawn from, most likely won't change
+	self.alpha = 0 -- The default alpha, 0 = invisible
+	self.fade = 0 -- The direction to fade, 0 = no fade actoin
 end
 
+--[
+--Adds a 'pose'; another image that can be drawn in the character's place
+--@param pose The name for the pose to be called from in the character's pose table
+--@param path The image path for the pose to point to
+--]
 function Character:addPose(pose, path)
 	self.poses[pose] = love.graphics.newImage(path)
 end
 
+--[
+--Draws a character at the alpha of the character
+--@param pick The character in the characters table to use
+--]
 function draw(pick)
 	love.graphics.setColor(br, br, br, characters[pick].alpha)
 	love.graphics.draw(characters[pick].image, characters[pick].x, characters[pick].y)
 	love.graphics.setColor(br, br, br, 255)
 end
 
+--[
+--Moves a character to a select position
+--@param chr The character to move
+--@param pos The position (left, right, middle) to move to the character
+--]
 function Move(chr, pos)
 	characters[chr].y = height - characters[chr].image:getHeight()
 	if pos == "right" then
@@ -52,6 +81,59 @@ function Move(chr, pos)
 	end
 end
 
+--[
+--Initializes the sequence for text to fade in, and wraps the text
+--]
+function printInit()
+	local space = 0
+	local tmpLength = 0
+	txtMax = oldTxt
+	txtLimit = width * .7
+	for j=oldTxt+1, string.len(txtDisplay) do
+		txtAlpha[j] = 0
+		ch = string.sub(txtDisplay, j, j)
+		txtMax = txtMax + 1
+		txtTable[txtMax] = ch
+		tmpLength = tmpLength + Font:getWidth(txtTable[txtMax])
+		if tmpLength > txtLimit then -- If the line goes over the limit, go back to last space and make it a line break
+			txtTable[space] = "\n"
+			tmpLength = 0
+			space = space + 1
+			while space < txtMax do
+				tmpLength = tmpLength  + Font:getWidth(txtTable[space])
+				space = space + 1
+			end
+		end
+		if ch == " " then space = txtMax end
+	end
+	txtAlpha[1] = 1
+end
+
+--[
+--Prints out the txtTable
+--]
+function Print()
+	local x = width * .15
+	local y = 0
+	for i=1, txtMax do
+		if txtTable[i] ~= nil then
+			if txtTable[i] == "\n" then
+				x = width * .15
+				y = y + Font:getHeight("T")
+			else
+				love.graphics.setColor(255, 255, 255, txtAlpha[i])
+				love.graphics.print(txtTable[i], x, y)
+				x = x + Font:getWidth(txtTable[i])
+			end
+		end
+	end
+end
+
+--[
+--Splits a string into words
+--@param s The string to split
+--@return The table of words from 0,...length-1
+--]
 function lineSplit(s)
 	split = {}
 	o = 0
@@ -62,7 +144,7 @@ function lineSplit(s)
 	return split
 end
 
--- Gets called for computing a script block that isn't simply adding text
+-- Interperets called for computing a script block that isn't simply adding text
 function computeScript()
 	cLine = cLine + 1
 	while line[cLine] ~= "--Conf" do
@@ -83,6 +165,8 @@ function computeScript()
 		-- Backgrounds
 		elseif lineS[0] == "Background" then
 			if lineS[2] == "set" then
+				sX = width / backgrounds[lineS[1]]:getWidth()
+				sY = height /  backgrounds[lineS[1]]:getHeight()
 				bg = backgrounds[lineS[1]]
 			end
 			if lineS[1] == "remove" then
@@ -122,6 +206,7 @@ function love.load()
 			end
 		elseif lineS[0] == "Background" then
 			backgrounds[lineS[1]] = love.graphics.newImage(lineS[2])
+			backgrounds[lineS[1]]:setFilter("nearest","nearest", 16)
 		elseif lineS[0] == "setResolution" then
 			success = love.window.setMode(lineS[1], lineS[2])
 		elseif lineS[0] == "setTitle" then
@@ -135,8 +220,8 @@ function love.load()
 			musics[lineS[1]]:setLooping(true)
 		-- Font	
 		elseif lineS[0] == "Font" then
-			font = love.graphics.newFont(tonumber(lineS[2]))
-			love.graphics.setFont(font)
+			Font = love.graphics.newFont(tonumber(lineS[2]))
+			love.graphics.setFont(Font)
 		-- Voice Table
 		elseif lineS[0] == "VoiceTable" then
 			for title, dir in pairs(love.filesystem.getDirectoryItems(lineS[1])) do
@@ -156,6 +241,7 @@ function love.load()
 	lineCount = cLine
 	cLine = 0
 	txtDisplay = line[0]
+	intp = true
 end
 
 function love.update(dt)
@@ -165,6 +251,10 @@ function love.update(dt)
 		txtDisplay = ""
 		drawableBg = {}
 		characters = {}
+		txtTable = {}
+		oldTxt = 1
+		txtAlpha =  {}
+		bg = nil
 		line = {}
 		br = 255
 		love.audio.stop()
@@ -190,34 +280,69 @@ function love.update(dt)
 			end
 		end
 	end
-end
-
-function love.mousepressed(x, y, button)
-	if button == 'l' and inpt == true then
-		-- Finish fade on player demand
-		for character,k in pairs(characters) do
-			if k.fade == -1 then
-				k.alpha = 0
-				k.fade = 0
-			elseif k.fade == 1 then
-				k.alpha = 255
-				k.fade = 1
+	-- Fade in text
+	for k = oldTxt, txtMax do
+		if txtAlpha[k] ~= nil then
+			if txtAlpha[k] > 0 and txtAlpha[k] < 255 then
+				txtAlpha[k] = txtAlpha[k] + (256 * dt * 2.6 / 1)
+			end
+			if txtAlpha[k] > 255 then txtAlpha[k] = 255 end
+			if txtAlpha[k] > 40 and txtAlpha[k+1] ~= nil and txtAlpha[k+1] == 0 then
+				txtAlpha[k+1] = 1
 			end
 		end
-		-- Voice Cancel/Refresh
-		if voicePlay then
-			voiceTable[voicePos-1]:stop()
-			voiceTable[voicePos]:play()
-			voicePos = voicePos + 1
-		end
-		-- Start traversing the next chunk of lines
-		cLine = cLine + 1
-		if line[cLine] == "--Clear" then
-			txtDisplay = ""
-		elseif line[cLine] == "--Conf" then
-			computeScript()
-		elseif line[cLine] ~= nill then
-			txtDisplay = txtDisplay .. "\n\n\n " .. line[cLine]
+	end
+	--if txtTable[txtMax-1].alpha == 255 then click = 1 end -- If all text is shown, player click should move to new line
+end
+
+--[
+--Takes user input as moving onto the next chunk of text
+--@param x The x-pos of the mouse
+--@param y The y-pos of the mouse
+--@button Which button is being pressed
+--]
+function love.mousepressed(x, y, button)
+	if inpt == true and button == 'l' then
+		if click == 0 then
+			-- Finish fade on player demand
+			for character,k in pairs(characters) do
+				if k.fade == -1 then
+					k.alpha = 0
+					k.fade = 0
+				elseif k.fade == 1 then
+					k.alpha = 255
+					k.fade = 1
+				end
+			end
+			-- Finish fade on text
+			for j=1, txtMax do
+				txtAlpha[j] = 255
+			end
+			click = 1
+		else -- Second click, fading et al is done
+			click = 0
+			-- Voice Cancel/Refresh
+			if voicePlay then
+				voiceTable[voicePos-1]:stop()
+				voiceTable[voicePos]:play()
+				voicePos = voicePos + 1
+			end
+			-- Start traversing the next chunk of lines
+			cLine = cLine + 1
+			if line[cLine] == "--Clear" then
+				txtDisplay = ""
+				oldTxt = 1
+				y = 0
+				printInit()
+			elseif line[cLine] == "--Conf" then
+				computeScript()
+			elseif line[cLine] ~= nill then
+				if txtDisplay ~= "" then
+					oldTxt = txtMax
+				end
+				txtDisplay = txtDisplay .. "\n" .."\n" .."\n" .. line[cLine]
+				printInit()
+			end
 		end
 	end
 end
@@ -232,13 +357,12 @@ function love.draw()
 	-- If there is a background then draw it
 	if bg ~= nil then
 		love.graphics.setColor(br, br, br, 255)
-		love.graphics.draw(bg, 0, 0)
+		love.graphics.draw(bg, 0, 0, 0, sX, sY)
 		love.graphics.setColor(255, 255, 255, 255)
 	end
 	for character,k in pairs(characters) do
 		draw(character)
 	end
-	love.graphics.setColor(255, 255, 255, 255)
-	love.graphics.printf(txtDisplay, width*.15, 0, .7 * width)
+	Print()
 	--love.graphics.print(cLine, 400, 100)
 end
